032b3076932d50fc668768bc17c8240cf9632b52
it21065262 <it21065262@my.sliit.lk>
2024-09-25 19:28:59 +0530

    1st commit

---
+++ app/src/main/java/com/example/mindwell/LandingPage.kt
@@ -0,0 +1,31 @@
+package com.example.mindwell
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+private val handler = Handler(Looper.getMainLooper())
+
+class LandingPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_landing_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        handler.postDelayed({
+            startActivity(Intent(this, LoginSignupPage::class.java))
+            finish()
+        }, 2500L)
+
+    }
+}
